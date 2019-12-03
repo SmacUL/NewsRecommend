1b3d2598566144e564fe807432a5aa079a1e8c85
SmacUL <1164253125@qq.com>
2019-12-03 11:24:30 +0800

    showpanel css 修改

--- front/src/components/self/ShowPanel.vue
+++ front/src/components/self/ShowPanel.vue
@@ -3,34 +3,27 @@
 <!--        <div style="background-image: url('@/assets/image/selfbackground.jpg');">-->
         <div :style="note">
             <!-- 用户信息的显示板 -->
-            <div
-                style="position: absolute; top: 150px; width: 100%;height: 150px; background-color: rgba(244, 245, 246, 0.7)">
+            <div class="customer">
                 <!-- 头像 -->
-                <div style="height: 100px; z-index: 1000; width: 100px; position: absolute; top: -50px; left: 3%;">
-                    <img style="width: 100%; " :src="customer.cusAvatarUrl" >
-                </div>
+                <div class="customer-avatar"><img :src="customer.cusAvatarUrl" ></div>
                 <!-- 信息 -->
-                <div style="position: relative;height: 150px;">
-                    <div style="position: absolute; margin-left: 3%; width: 17%; margin-top: 70px; height: 80px; text-align: left">
-                        <div>
-                            <span>{{customer.cusName}}</span>
-                            <span>{{customer.cusGender}}</span>
-                        </div>
-
+                <div class="customer-info">
+                    <div class="customer-info-left">
+                        <div><span>{{customer.cusName}}</span><span>{{customer.cusGender}}</span></div>
                         <el-button class="follow" type="primary">+关注</el-button>
                     </div>
-                    <div style="position: absolute;margin-left: 20%; width: 80%; height: 150px; text-align: left;">
-                        <span style="display: block; font-size: 20px; line-height: 48px;">{{ customer.cusStyle}}</span>
-                        <div style="display: flex; text-align: right">
-                            <div style="width: 70px; margin-right: 30px;">
-                                <span style="display: block">Email</span>
-                                <span style="display: block">Phone</span>
-                                <span style="display: block">地址</span>
+                    <div class="customer-info-right">
+                        <span class="customer-info-style">{{ customer.cusStyle}}</span>
+                        <div class="customer-info-panel">
+                            <div>
+                                <span>Email</span>
+                                <span>Phone</span>
+                                <span>地址</span>
                             </div>
-                            <div style="width: 250px; text-align: left">
-                                <span style="display: block">{{ customer.cusEmail }}</span>
-                                <span style="display: block">{{ customer.cusPhone }}</span>
-                                <span style="display: block">{{ customer.cusAddress }}</span>
+                            <div>
+                                <span>{{ customer.cusEmail }}</span>
+                                <span>{{ customer.cusPhone }}</span>
+                                <span>{{ customer.cusAddress }}</span>
                             </div>
                         </div>
                     </div>
@@ -61,13 +54,81 @@ export default {
 }
 </script>
 
-<style>
-    .show-panel .follow.el-button--primary {
+<style scoped>
+    .follow.el-button--primary {
         height: 20px;
         width: 60px;
         padding: 0px;
         font-size: 10pt;
         border-radius: 1px;
     }
+
+    .customer {
+        position: absolute;
+        top: 150px;
+        width: 100%;
+        height: 150px;
+        background-color: rgba(244, 245, 246, 0.7);
+    }
+
+    .customer-avatar {
+        height: 100px;
+        z-index: 1000;
+        width: 100px;
+        position: absolute;
+        top: -50px; left: 3%;
+    }
+
+    .customer-avatar img {
+        width: 100%;
+    }
+
+    .customer-info {
+        position: relative;
+        height: 150px;
+    }
+
+    .customer-info-left {
+        position: absolute;
+        margin-left: 3%;
+        margin-top: 70px;
+        width: 17%;
+        height: 80px;
+        text-align: left;
+    }
+
+    .customer-info-right {
+        position: absolute;
+        margin-left: 20%;
+        width: 80%;
+        height: 150px;
+        text-align: left;
+    }
+
+    .customer-info-style {
+        display: block;
+        font-size: 20px;
+        line-height: 48px;
+    }
+
+    .customer-info-panel {
+        display: flex;
+    }
+
+    .customer-info-panel div:nth-child(1) {
+        width: 70px;
+        margin-right: 30px;
+        text-align: right;
+    }
+
+    .customer-info-panel div:nth-child(2) {
+        width: 250px;
+        text-align: left;
+    }
+
+    .customer-info-panel span {
+        display: inline-block;
+    }
+
 
 </style>
